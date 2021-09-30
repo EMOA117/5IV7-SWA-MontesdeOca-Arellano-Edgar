a91a5499c9215d5b5899140110bb6c05ee106550
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2021-09-30 17:23:25 -0500

    cifrado vigenere

--- 02 Vigenere/js/vigenere.js
+++ 02 Vigenere/js/vigenere.js
@@ -1,133 +1,109 @@
-const abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ',
-             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z'];
 
+//llave
 let key = "";
+
 
 $(document).ready(function() {
     $('#ci').click(function() {
-
+        //aquí vamos a cifrar
         key = document.getElementById('llave').value;
-
         key = key.replace(/ /g, '');
-
         let mess = document.getElementById('mess').value;
-
         mess = mess.replace(/ /g, '');
-
         let newMess = "";
-
         let keyComplete = "";
-
-        if(revision(mess, key)) {
-
-            for(var i = 0; i<mess.length; i++) {
+        //Aquí empieza el algoritmo
+        if (revision(mess, key)) {
+            for (var i = 0; i < mess.length; i++) {
                 keyComplete += key.charAt(i%Number(key.length));
-
             }
-            alert(keyComplete);
-
-            for(var i = 0; i<mess.length; i++) {
-                
+            for (var i = 0; i < mess.length; i++) {
+                //obtener la posicion de la letra por letra del mensaje
                 let charr = mess.charAt(i);
-                let posn = getPosition(charr);
-
+                let posm = getPosition(charr);
                 charr = keyComplete.charAt(i);
                 let posk = getPosition(charr);
-
-                let newVal = change(posn, posk);
+                let newVal = change(posm, posk);
                 newMess += abc[newVal];
             }
-
-            document.getElementById('rs').value = newMess;
-
-        }else{
-
+            document.getElementById("rs").innerHTML = newMess; 
         }
-
-
     });
-
     $('#de').click(function() {
-
+        //para cifrar vamos a usar la funcion
+        // y = (x+z)mod27 pq estamos usando la ñ
+        //vamos a traer los datos de los campos de texto
         key = document.getElementById('llave').value;
-
+        //vamos a verificar los datos
         key = key.replace(/ /g, '');
-
+        //obtener el mensaje
         let mess = document.getElementById('mess').value;
-
         mess = mess.replace(/ /g, '');
-
         let newMess = "";
-
         let keyComplete = "";
-
-        if(revision(mess, key)) {
-
-            for(var i = 0; i<mess.length; i++) {
-                keyComplete += key.charAt(i%Number(key.length));
-
+        //algoritmo
+        if (revision(mess, key)) {
+            for (var i = 0; i < mess.length; i++) {
+                keyComplete += key.charAt((i%Number(key.length)));
             }
-            alert(keyComplete);
-
-            for(var i = 0; i<mess.length; i++) {
-                
-                let charr = mess.charAt(i);
-                let posn = getPosition(charr);
-
+            for (var i = 0; i < mess.length; i++) {
+                //obtener la poscion de la letra por letra del mensaje
+                let charr = keyComplete.charAt(i);
+                let posm = getPosition(charr);
                 charr = keyComplete.charAt(i);
                 let posk = getPosition(charr);
-
-                let newVal = change(posn, posk);
-                newMess += abc[newVal];
+                //ejecutamos el algoritmo
+                let newVal = rechange(posm, posk);
+                newMess += abc[newVal]; //mensaje decifrado
             }
-
-            document.getElementById('rs').value = newMess;
-
-        }else{
-
+            //imprimir el resultado
+            document.getElementById("rs").innerHTML = newMess;
         }
-
-
     });
-
-
 });
 
-function change(posm, posk){
+
+
+
+function change(posm, posk) {
+    // y = (x + z)mod27
     let y = (posm+posk)%27;
     return y;
 }
 
-function rechange(posm, posk){
+function rechange(posm, posk) {
     let val = 0;
-    if((posm-posk)>=0){
+    if ((posm-posk) >= 0) {
         val = (posm+posk)%27;
-    }else{
+    } else {
         val = (posm-posk+27)%27;
     }
     return val;
 }
 
-function getPosition(letra){
+function getPosition(letra) {
     let position = abc.indexOf(letra);
     return position;
 }
 
-function revision(mess, desp){
-    const re = /^([a-zñ?]+([]*[a-zñ?]?['-]?[a-zñ]+)*)$/
+function revision(mess, desp) {
+    const re = /^([a-zñ?]+([]*[a-zñ?]?['-]?[a-zñ?]+)*)$/
 
     var acc = true;
+
     if(!re.test(mess)){
         sd();
         acc = false;
     }
-
     if(!re.test(desp)){
         sdd();
         acc = false;
     }
-    if(desp.lenght > message.lenght){
-        sz(); 
+    if(desp.length > mess.length){
+        sz();
     }
     return acc;
 }
